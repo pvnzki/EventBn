8d74595dc23e2b988cfd4c42df11177583b4242b
Matheesha Fernando <fernandomatheesha@gmail.com>
2025-08-07 10:30:02 +0530

    commit

--- backend-api/server.js
+++ backend-api/server.js
@@ -15,6 +15,7 @@ const corsOptions = {
   origin: process.env.CORS_ORIGIN?.split(",") || [
     "http://localhost:3000",
     "http://10.0.2.2:3000", // Android emulator
+    "http://192.168.1.104:3000"
   ],
   credentials: true,
   optionsSuccessStatus: 200,
