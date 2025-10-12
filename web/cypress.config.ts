096a1fa6ee3702da801b66eaafd3cefbe6108f34
Matheesha Fernando <fernandomatheesha@gmail.com>
2025-10-12 17:24:07 +0530

    eslint configuration

--- web/cypress.config.ts
+++ web/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3001",
+    baseUrl: "http://localhost:5000",
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     supportFile: "cypress/support/e2e.ts",
     video: false,
